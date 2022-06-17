9a83070a07c4a25489dc96a3283c9561125501b2
MrJake222 <norbertzpilicy@gmail.com>
2022-06-17 02:00:53 +0200

    Changed test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,22 @@
 OUT=out.bin
 DECODED=decoded.txt
 
-FILES=$(find txt -type f)
+FILES=$(find txt -type f | sort)
 
 for FILE in ${FILES}; do
-    echo Trying ${FILE}
-    ./main --pack -s ${FILE} -d ${OUT} > /dev/null
-    ./main --unpack -s ${OUT} -d ${DECODED} > /dev/null
-        
+    # echo Trying ${FILE}
+    ./main --pack -s ${FILE} -d ${OUT} # > /dev/null
+    ./main --unpack -s ${OUT} -d ${DECODED} # > /dev/null
+
     SUM_ORIG=($(md5sum ${FILE}))
     SUM_DECODED=($(md5sum ${DECODED}))
-        
+
     if [[ "${SUM_ORIG}" == "${SUM_DECODED}" ]]; then
         echo "OK"
     else
         echo "FAIL"
     fi
-    
+
+    echo ""
     rm ${OUT} ${DECODED}
 done
